d32e2066bf719039ebcc495b4dd886e9db564461
zx <102405740+KimRRRich@users.noreply.github.com>
2022-11-09 21:20:49 +0800

    11.9update

--- Assets/Scripts/Conf/SkillModel.cs
+++ Assets/Scripts/Conf/SkillModel.cs
@@ -9,8 +9,33 @@ public class SkillModel
 {
     public Conf_SkillData SkillData;
     public KeyCode KeyCode;
-    public float CDTime;
-    public float CurrTime;
+    public float CDTime;      //CD时间
+    private float CurrTime=0; //0为CD结束的意思
     public Image CDimage;
+
+    public bool canRelease { get; private set; } = true;
+    public void Update()
+    {
+        //不能释放
+        if (canRelease == false && CurrTime > 0)
+        {
+            CurrTime -= Time.deltaTime;
+            //CD结束
+            if (CurrTime <=0)
+            {
+                CurrTime = 0;
+                canRelease = true;
+            }
+            //更新UI
+            CDimage.fillAmount = CurrTime / CDTime;
+        }
+    }
+
+    //释放的时候
+    public void OnRelese()
+    {
+        canRelease = false;
+        CurrTime = CDTime;
+    }
 
 }
--- Assets/Scripts/Player/Player_Controller.cs
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -58,19 +58,7 @@ public class Player_Controller : FSMControl<PlayerState>
     //技能的包装数据cd、按键
     public SkillModel[] SkillModels;
 
-    // 当前的技能
-    public Conf_SkillData CurrSkillData { get; private set; }
-    // 当前是第几段攻击
-    private int currAttackIndex = 0;
-    public int CurrAttackIndex
-    {
-        get => currAttackIndex;
-        set
-        {
-            if (value == StandAttackConfs.Length) currAttackIndex = 0;
-            else currAttackIndex = value;
-        }
-    }
+    
 
 
 
@@ -96,29 +84,58 @@ public class Player_Controller : FSMControl<PlayerState>
     protected override void Update()
     {
         base.Update();
+        UpdateSkillCD();
         if (input.GetKeyDown(KeyCode.Space))
         {
             Hp -= UnityEngine.Random.Range(1, 20);
         }
     }
 
+    //更新技能CD
+    private void UpdateSkillCD()
+    {
+        for(int i = 0; i < SkillModels.Length; i++)
+        {
+            SkillModels[i].Update();
+        }
+    }
+
+    #region  战斗相关
+    // 当前的技能
+    public Conf_SkillData CurrSkillData { get; private set; }
+    // 当前是第几段攻击(普攻)
+    private int currAttackIndex = 0;
+    public int CurrAttackIndex
+    {
+        get => currAttackIndex;
+        set
+        {
+            if (value == StandAttackConfs.Length) currAttackIndex = 0;
+            else currAttackIndex = value;
+        }
+    }
+
+    //当前的技能编号
+    private int currSkillIndex = -1;
     // 检查攻击状态
     public bool CheckAttack()
     {
         //普攻检测
-        if(input.GetAttackKeyDown() && model.canSwitch)
+        if (input.GetAttackKeyDown() && model.canSwitch)
         {
             CurrSkillData = StandAttackConfs[CurrAttackIndex];
             AttackAction = StandAttack;
+            currSkillIndex = -1;
             return true;
         }
         //技能检测
-        for(int i = 0; i < SkillModels.Length; i++)
+        for (int i = 0; i < SkillModels.Length; i++)
         {
-            if (input.GetKeyDown(SkillModels[i].KeyCode) && model.canSwitch)
+            if (input.GetKeyDown(SkillModels[i].KeyCode) &&SkillModels[i].canRelease &&model.canSwitch)
             {
                 CurrSkillData = SkillModels[i].SkillData;
                 AttackAction = SkillAttack;
+                currSkillIndex = i;
                 return true;
             }
         }
@@ -133,6 +150,10 @@ public class Player_Controller : FSMControl<PlayerState>
     public void Attack()
     {
         AttackAction?.Invoke();//不是null就执行
+        if (currSkillIndex != -1)
+        {
+            SkillModels[currSkillIndex].OnRelese();
+        }
     }
 
     //普通攻击
@@ -163,21 +184,21 @@ public class Player_Controller : FSMControl<PlayerState>
     }
 
     //基于攻击的相机移动
-    public void CameraMoveForAttack(Vector3 offset,float time,float backTime)
+    public void CameraMoveForAttack(Vector3 offset, float time, float backTime)
     {
         // 花time的时间去offset的位置
-        cameraTarget.DOLocalMove(cameraPos + offset, time).onComplete=()=>
+        cameraTarget.DOLocalMove(cameraPos + offset, time).onComplete = () =>
         {
             //经过backTime的时间回归到原来的位置
             cameraTarget.DOLocalMove(cameraPos, backTime);
         };
-        
+
     }
 
     /// <summary>
     /// 角色攻击移动
     /// </summary>
-    public void CharacterAttackMove(Vector3 target,float time)
+    public void CharacterAttackMove(Vector3 target, float time)
     {
         StartCoroutine(DoCharacterAttackMove(transform.TransformDirection(target), time));
     }
@@ -185,7 +206,7 @@ public class Player_Controller : FSMControl<PlayerState>
     IEnumerator DoCharacterAttackMove(Vector3 target, float time)
     {
         float currTime = 0;
-        while (currTime<time)
+        while (currTime < time)
         {
             Vector3 moveDir = target * Time.deltaTime / time;
             characterController.Move(moveDir);
@@ -195,4 +216,7 @@ public class Player_Controller : FSMControl<PlayerState>
         //暂停一帧
         yield return null;
     }
+    #endregion
+
+
 }
